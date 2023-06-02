15c0011e7f78810725f56dc96565a20355bb6788
bei03 <beafea.bma@gmail.com>
2023-06-02 18:41:49 +0200

    javascript

---
+++ PrimerJS/js/script.js
@@ -0,0 +1,21 @@
+alert("-Hola a todo el mundo :) -Hola Doctor Nick")
+//Comentarios de línea
+/*Comentarios de bloque
+*/
+
+let nombre="Beita Mena";
+let parrafo=document.getElementById("parrafo");parrafo.innerHTML=nombre;
+console.log(nombre);
+
+document.getElementById("demo") .innerHTML="Qué pasa lokis";
+
+//Variables tipo entero
+let numero1=45;
+let numero2=25;
+let suma=45+25;
+console.log(suma);
+
+let apellido="Almansa";
+nombre=nombre+" "+apellido;
+console.log(nombre);
+document.getElementById("nuevoNombre").innerHTML=nombre;

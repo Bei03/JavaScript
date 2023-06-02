41ac9dce130fef231f0e7021e3f334bffae6b79d
bei03 <beafea.bma@gmail.com>
2023-06-02 20:53:33 +0200

    primerdiajava

--- PrimerJS/js/script.js
+++ PrimerJS/js/script.js
@@ -19,3 +19,37 @@ let apellido="Almansa";
 nombre=nombre+" "+apellido;
 console.log(nombre);
 document.getElementById("nuevoNombre").innerHTML=nombre;
+
+numero1="hola"
+suma=numero1+numero2;
+console.log(suma); //forma de visualizar una salida en javascrpt
+
+function mostrarAlerta(){
+    alert("Cuidado!Radiactivo Man");
+document.getElementById("parrafo2").innerHTML= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error molestiae, libero praesentium impedit vel sequi voluptates eaque cumque deserunt non dolorem quis perspiciatis voluptatibus nisi quos nemo quod temporibus totam.";
+}
+
+
+document.getElementById("suma").innerHTML=sumar(32,12)
+
+function sumar(a,b){
+return a+b
+}
+
+
+
+
+function sumar2(){
+//leemos del formulario los valores que ingresen
+//casting: cambiar de un tipo a otro... caracteres->numeros
+let x= parseInt(document.getElementById("numero1").value);
+let y= parseInt(document.getElementById("numero2").value);
+
+document.getElementById("suma2").innerHTML= "El valor de x es" + x + " .El valor de y es " + y + " y la suma de estos dos valores es " + (x+y);
+}
+
+
+document.getElementById("suma").innerHTML=sumar(45, 23);
+function sumar (a,b){
+    return a+b;
+}
